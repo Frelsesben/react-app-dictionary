507ec9cc74374a612ba4f57fac20f0de9428b842
Frelsesben <frelsesben@gmail.com>
2022-08-21 13:25:28 +0200

    updated so only show one definition pr type of word + synonyms
    Replaced the map to only show [0] as is a more clean look

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,21 +1,19 @@
 import React from "react";
 import "./styles.css";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
+  console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function (definition, index) {
-        return (
-          <div key={index}>
-            <p>
-              <b>{definition.definition}</b>
-              <br />
-              <i>{definition.example}</i>
-            </p>
-          </div>
-        );
-      })}
+      <div>
+        <b>1. {props.meaning.definitions[0].definition}</b>
+        <br />
+        <i>{props.meaning.definitions[0].example}</i>
+        <hr />
+        <Synonyms synonyms={props.meaning.synonyms} />
+      </div>
     </div>
   );
 }
